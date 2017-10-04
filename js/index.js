42ce0f03e4b0e3f85b92c85896baef18254602d1
Braedin Jared <braedinjared@gmail.com>
2017-10-04 18:46:13 -0500

    Initial

---
+++ js/index.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    
+    var gridSize = 50;
+
+    var gridSet = {
+        //lineColor: '#FEF101',
+        lineColor: '#FFFFFF',
+        rowLineCount: 20,
+        columnLineCount: 32,
+        opacity: .3,
+        lineDuration: 150,
+        interval: 50
+    };
+    
+    var ctx = $('#video-grid').get(0).getContext('2d');
+
+    var height = $('#video-grid').innerHeight();
+    var width = $('#video-grid').innerWidth();
+    //var height = $('#video-grid').outerHeight();
+    //var width = $('#video-grid').outerWidth();
+    
+    var grid = new GridCanvas(ctx, height, width, gridSet);
+    grid.start();
+    
+    $(window).on('resize', function(){
+        resizeCanvas();
+        resizeLatest();
+    });
+
+    function resizeCanvas(){
+        var parent = $('#video-grid').parent();
+
+        $('#video-grid').get(0).width = parent.width();
+        $('#video-grid').get(0).height = parent.height();
+
+        var height = $('#video-grid').innerHeight();
+        var width = $('#video-grid').innerWidth();
+        //var height = $('#video-grid').outerHeight();
+        //var width = $('#video-grid').outerWidth();
+
+        $('#video-grid').get(0).width = parent.width();
+        $('#video-grid').get(0).height = parent.height();
+
+        //height = $('#video-grid').innerHeight();
+        //width = $('#video-grid').innerWidth();
+        //height = $('#video-grid').outerHeight();
+        //width = $('#video-grid').outerWidth();
+
+
+        grid.updateRes(height, width);
+
+        var rc = Math.ceil(height/gridSize);
+        var cc = Math.ceil(width/gridSize);
+        // 1920/20 = 96
+
+        var newSet = {
+            rowLineCount: rc,
+            columnLineCount: cc
+        };
+        grid.updateSettings(newSet);
+    }
+
+    resizeCanvas();
+
+    $('.parallax-window').parallax();
+
+    $('#corridor-title').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
+        $('#corridor-sub-title').fadeIn();
+    });
+
+
+
+    function resizeLatest(){
+        //$('#latest-video').width();
+    }
+});
